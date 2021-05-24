ef17b63f674e4e1dabdb555c26d14ef692fc0a99
Tobias <tobias.ku22@gmail.com>
2021-05-24 20:18:38 +0200

    smaller changes and CI/CD demo

--- src/main/java/com/example/demo/Service/IUserClassService.java
+++ src/main/java/com/example/demo/Service/IUserClassService.java
@@ -1,9 +1,11 @@
 package com.example.demo.Service;
 
 import com.example.demo.Model.UserClass;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public interface IUserClassService {
 
     void save(UserClass userClass);
